fd8fd2cfac24998af4408ed6053f111338a8116d
jacoby3210 <158810972+jacoby3210@users.noreply.github.com>
2024-07-05 07:18:46 +0300

    Initial commit

---
+++ src/gui/main.jsx
@@ -0,0 +1,15 @@
+// Router to control navigation through the application pages.
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Main() {
+
+	return (
+		<>
+			<Router></Router>
+		</>
+	);
+}
+
+export default React.memo(Main);
