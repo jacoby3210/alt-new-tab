6ef6bd42cf675568a18a060f91da7ff098a62db7
Alexandr <jacoby3210@gmail.com>
2024-07-13 08:00:47 +0300

    update test page

--- src/gui/pages/test/test.jsx
+++ src/gui/pages/test/test.jsx
@@ -5,35 +5,41 @@ import Common from "react-common"
 const Test = (props) => {
 
 	//  helpers
-	const produceEntries = (count, func) => {return Array.from(new Array(count), func);}
-	const area = React.useRef(null)
-	const TestAreaForScroll = () => 
-		<div style={{height:"100px", "overflow-y":"scroll"}} ref={area}>
-			<div style={{height:"200px"}}/>
-		</div>
+	const produceEntries = (count, func) => { return Array.from(new Array(count), func); }
 
-	const scrollProps = {
-		target: area,
-	}
 	// basics
-	const pathProps = {data: "/test/3",};
-	const popupProps = {shown: true,};
-	
-	// advisor
-	const advisorProps = {
-		data: produceEntries(5, (v, i) => {return { caption: `Option #${i}`, value: i}}),
-	}
+	const pathProps = { data: "/test/3", };
+	const popupProps = { shown: true, };
 
 	// dropdown
 	const dropdownProps = {
-		data: produceEntries(5, (v, i) => {return { caption: `Option #${i}`, value: i}}),
+		data: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, value: i } }),
 		value: 0
 	}
 
-	// Page Controller
+	// scroll
+	const area = React.useRef(null)
+	const TestAreaForScroll = () =>
+		<div style={{ height: "100px", "overflowY": "scroll" }} ref={area}>
+			<div style={{ height: "200px" }} />
+		</div>
+
+	const scrollProps = { target: area, }
+
+	// advisor
+	const advisorProps = {
+		data: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, value: i } }),
+	}
+
+	// accordion
+	const accordionProps = {
+		data: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, content: i } }),
+	}
+
+	// page controller
 	const [currentPage, setCurrentPage] = useState(0);
 	const pageControllerProps = {
-		onChangeCallback: (pageIndex) => { setCurrentPage(pageIndex * 10);},
+		onChangeCallback: (pageIndex) => { setCurrentPage(pageIndex * 10); },
 		buttons: 5,
 		count: 250 / 10,
 		value: 0,
@@ -41,15 +47,15 @@ const Test = (props) => {
 	const viewPropsForPageController = {
 		count: 250,
 		from: currentPage,
-		data: Array.from({ length: 250 }, (_, i) => {return { text: `string_${i}`}}),
+		data: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
 		range: 10,
 	}
 
-	// Slider Controller
+	// slider controller
 	const [currentCarouselSlide, setCurrentCarouselSlide] = useState(0);
 	const carouselControllerProps = {
 		infinity: true,
-		onChangeCallback: (slideIndex) => { setCurrentCarouselSlide(slideIndex);},
+		onChangeCallback: (slideIndex) => { setCurrentCarouselSlide(slideIndex); },
 		count: 250,
 	}
 	const viewPropsForCarouselController = {
@@ -59,10 +65,10 @@ const Test = (props) => {
 		range: 1,
 	}
 
-	// Tab Controller
+	// tab controller
 	const [currentTab, setCurrentTab] = useState(0);
 	const tabControllerProps = {
-		data: produceEntries(5, (v, i) => {return { caption: `Option #${i}`, id: i}}),
+		data: produceEntries(5, (v, i) => { return { caption: `Option #${i}`, id: i } }),
 		onChangeCallback: (tabIndex) => { setCurrentTab(tabIndex) },
 	}
 	const viewPropsForTabController = {
@@ -71,26 +77,28 @@ const Test = (props) => {
 		data: Array.from({ length: 250 }, (_, i) => { return { text: `string_${i}` } }),
 		range: 1,
 	}
-	
+
 	// render page
 	return (
 		<>
 			<Common.Path {...pathProps} />
 			<Common.Popup {...popupProps} />
-			<Common.Advisor {...advisorProps} />
+
 			<Common.Dropdown {...dropdownProps} />
+			<Common.Scroll {...scrollProps} />
+			<TestAreaForScroll />
 
-			<TestAreaForScroll/>
-			<Common.Scroll {...scrollProps}/>
+			<Common.Advisor {...advisorProps} />
 
-			<Common.CarouselController {...carouselControllerProps}/>
-			<Common.View  {...viewPropsForCarouselController}/>
+			<Common.AccordionView {...accordionProps} />
+			<Common.CarouselController {...carouselControllerProps} />
+			<Common.View  {...viewPropsForCarouselController} />
 
-			<Common.PageController {...pageControllerProps}/>
-			<Common.View  {...viewPropsForPageController}/>
+			<Common.PageController {...pageControllerProps} />
+			<Common.View  {...viewPropsForPageController} />
 
-			<Common.TabController {...tabControllerProps}/>
-			<Common.View  {...viewPropsForTabController}/>
+			<Common.TabController {...tabControllerProps} />
+			<Common.View  {...viewPropsForTabController} />
 		</>
 	);
 }
